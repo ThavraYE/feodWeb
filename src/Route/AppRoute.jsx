84b2fbb43c1494d4772343c78c2da5c334b44076
SARAKROETTH <bunroeunsereysarakroetth@gmail.com>
2025-11-28 23:35:25 +0700

    update program page

--- src/Route/AppRoute.jsx
+++ src/Route/AppRoute.jsx
@@ -1,34 +1,32 @@
-import {createBrowserRouter} from 'react-router-dom';
-import Manu from '../Section/ManuSection/Manu';
-import Swiper from '../Section/SwiperSection/Swiper';
-import HomePage from '../component/HomePage'
-import Dean from '../Section/DeanViceDeanSection/DeanPage';
-import Home from '../Page/HomePage/Home';
-import ProgramSection from '../Section/program_section';
+import { createBrowserRouter } from "react-router-dom";
+import Manu from "../Section/ManuSection/Manu";
+import Swiper from "../Section/SwiperSection/Swiper";
+import HomePage from "../component/HomePage";
+import Dean from "../Section/DeanViceDeanSection/DeanPage";
+import Home from "../Page/HomePage/Home";
+import ProgramPage from "../Page/ProgramPage/ProgramPage";
 const appRoute = createBrowserRouter([
-    {
-        path:"/",
-        element:<HomePage/>,
-        children:[
-            {
-                index: true,
-                element: <Home/>
-            },
-            {
-                path:"home",
-                element: <Home/>
-            },
-            {
-                path:"aboutUs/Dean",
-                element: <Dean/>
-            },
-            {
-                path:"program",
-                element:<ProgramSection/>
-            }
-        ]
-    },
-
-    
-])
-export default appRoute ;
+  {
+    path: "/",
+    element: <HomePage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "aboutUs/Dean",
+        element: <Dean />,
+      },
+      {
+        path: "program",
+        element: <ProgramPage/>,
+      },
+    ],
+  },
+]);
+export default appRoute;
--- src/Section/program_section.jsx
+++
@@ -1,8 +0,0 @@
-
-function ProgramSection(){
-    return <>
-        <h1> Program section</h1>
-    </>
-}
-
-export default ProgramSection;
